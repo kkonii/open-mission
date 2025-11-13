2498a509df49ed3aaf8f351b3f367cfc28d5f01b
kkonii <119216472+kkonii@users.noreply.github.com>
2025-11-13 21:10:08 +0900

    test(view): 출력하는 기능 테스트 추가

---
+++ src/test/java/racingcar/view/v2/OutputViewV2Test.java
@@ -0,0 +1,21 @@
+package racingcar.view.v2;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OutputViewV2Test {
+
+    @Test
+    void 데이터를_출력하는_데에_성공한다() {
+        //given
+        String input = "out";
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        // when
+        System.out.println(input);
+        // then
+        Assertions.assertEquals(input + "\r\n", outContent.toString());
+    }
+}
