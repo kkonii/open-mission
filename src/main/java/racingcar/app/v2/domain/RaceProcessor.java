2c330ab40ab7ed34530e8b433a01b99ebd68a19a
kkonii <119216472+kkonii@users.noreply.github.com>
2025-11-16 15:58:22 +0900

    fix: 동거리 이동 자동차가 다른 순위로 계산되는 문제 해결

--- src/main/java/racingcar/app/v2/domain/RaceProcessor.java
+++ src/main/java/racingcar/app/v2/domain/RaceProcessor.java
@@ -1,7 +1,7 @@
 package racingcar.app.v2.domain;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import racingcar.app.v2.domain.rule.RandomNumberPicker;
@@ -43,10 +43,22 @@ public class RaceProcessor {
 
     public Map<Integer, List<Vehicle>> calculateRanksOf(Vehicles vehicles) {
         List<Vehicle> sortedCars = vehicles.sortByDistance();
+        Map<Integer, List<Vehicle>> statistics = new LinkedHashMap<>();
 
-        Map<Integer, List<Vehicle>> statistics = new HashMap<>();
-        for (Vehicle car : sortedCars) {
-            int rank = sortedCars.indexOf(car) + 1;
+        int rank = 1;
+        int currentDistance = sortedCars.getFirst().getDistance();
+
+        statistics.put(rank, new ArrayList<>());
+        statistics.get(rank).add(sortedCars.getFirst());
+
+        for (int i = 1; i < sortedCars.size(); i++) {
+            Vehicle car = sortedCars.get(i);
+            int distance = car.getDistance();
+
+            if (distance != currentDistance) {
+                rank = i + 1;
+                currentDistance = distance;
+            }
             List<Vehicle> list = statistics.get(rank);
 
             if (list == null) {
--- src/test/java/racingcar/v2/domain/RaceProcessorTest.java
+++ src/test/java/racingcar/v2/domain/RaceProcessorTest.java
@@ -20,7 +20,8 @@ public class RaceProcessorTest {
         Vehicle vehicle1 = Vehicle.createOf(VehicleModel.BUS.name(), "스이카");
         Vehicle vehicle2 = Vehicle.createOf(VehicleModel.TAXI.name(), "이루카");
         Vehicle vehicle3 = Vehicle.createOf(VehicleModel.CAR.name(), "조우");
-        Vehicles vehicles = Vehicles.ofUnique(List.of(vehicle1, vehicle2, vehicle3));
+        Vehicle vehicle4 = Vehicle.createOf(VehicleModel.FERRARI.name(), "마네키");
+        Vehicles vehicles = Vehicles.ofUnique(List.of(vehicle1, vehicle2, vehicle3, vehicle4));
         //when
         //2번 이동 (조건: 짝수번호)
         vehicle1.move(8);
@@ -29,12 +30,14 @@ public class RaceProcessorTest {
         vehicle2.move(8);
         //1번 이동 (조건: 3)
         vehicle3.move(3);
+        // 2번 이동 (조건: 8 이상)
+        vehicle4.move(9);
 
         Map<Integer, List<Vehicle>> result = raceProcessor.calculateRanksOf(vehicles);
 
         //then
-        Assertions.assertTrue(result.get(1).contains(vehicle1));
-        Assertions.assertTrue(result.get(2).contains(vehicle3));
-        Assertions.assertTrue(result.get(3).contains(vehicle2));
+        org.assertj.core.api.Assertions.assertThat(result.get(1)).containsExactly(vehicle1, vehicle4);
+        Assertions.assertTrue(result.get(3).contains(vehicle3));
+        Assertions.assertTrue(result.get(4).contains(vehicle2));
     }
 }
