0c18b81b1bcac73810a66ddce62c32d508bfcea3
kkonii <119216472+kkonii@users.noreply.github.com>
2025-11-17 14:15:26 +0900

    feat: 선택할 언어를 입력받는 기능 추가
    - 유효하지 않은 언어번호가 입력되었을 경우의 예외처리도 함께 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,27 @@
 package racingcar;
 
 import racingcar.app.v2.system.Language;
+import racingcar.app.v2.util.Parser;
+import racingcar.common.ConsoleScanner;
 import racingcar.common.Race;
+import racingcar.common.RetryHandler;
 import racingcar.config.AppConfig;
 
 public class Application {
     public static void main(String[] args) {
-        Race race = AppConfig.extendRace(Language.JAPANESE);
+        Language language = RetryHandler.runUntilSuccess(Application::chooseLanguage);
+        Race race = AppConfig.extendRace(language);
         race.run();
+    }
+
+    private static Language chooseLanguage() {
+        System.out.println("언어를 선택하세요 / Choose language / 言語を選択してください");
+        System.out.println("1. 한국어");
+        System.out.println("2. English");
+        System.out.println("3. 日本語");
+        
+        int languageNumber = Parser.toInteger(ConsoleScanner.readLine());
+
+        return Language.findBy(languageNumber);
     }
 }
