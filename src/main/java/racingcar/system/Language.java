d6e0e3b26410933d995948db13c75f13982f5b4c
kkonii <119216472+kkonii@users.noreply.github.com>
2025-11-18 18:11:13 +0900

    refactor(message): 언어별 출력 문구를 외부파일로 분리 및 locale 을 도입하여 전환하돌록 변경
    - 하드코딩된 출력 문구를 삭제
    - 언어별 문구를 properties 파일로 분리
    - Language enum에 Locale 필드를 추가하여 이를 통해 문구를 fetch 하도록 변경

--- src/main/java/racingcar/system/Language.java
+++ src/main/java/racingcar/system/Language.java
@@ -1,17 +1,20 @@
 package racingcar.system;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 public enum Language {
 
-    KOREAN(1),
-    ENGLISH(2),
-    JAPANESE(3);
+    KOREAN(1, Locale.of("ko", "KR")),
+    ENGLISH(2, Locale.of("en", "US")),
+    JAPANESE(3, Locale.of("ja", "JP"));
 
     private final int number;
+    private final Locale locale;
 
-    Language(int number) {
+    Language(int number, Locale locale) {
         this.number = number;
+        this.locale = locale;
     }
 
     public static Language findBy(int number) {
@@ -19,5 +22,9 @@ public enum Language {
                 .filter(lang -> lang.number == number)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 입력입니다. 다시 입력해주세요."));
+    }
+
+    public Locale locale() {
+        return this.locale;
     }
 }
