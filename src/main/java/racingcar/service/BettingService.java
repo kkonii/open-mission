1e7dc18f23de81ee6c7d9670ea71f8822369effd
kkonii <119216472+kkonii@users.noreply.github.com>
2025-11-21 17:28:13 +0900

    feat: 우승자 예측에 성공했는지 판별하는 기능 추가

--- src/main/java/racingcar/service/BettingService.java
+++ src/main/java/racingcar/service/BettingService.java
@@ -3,6 +3,7 @@ package racingcar.service;
 import java.util.List;
 import racingcar.domain.RaceProcessor;
 import racingcar.domain.Vehicles;
+import racingcar.domain.vo.PredictedWinner;
 import racingcar.dto.RaceResultDto;
 import racingcar.dto.RankResultDto;
 import racingcar.repository.BettingRoundRepository;
@@ -17,9 +18,10 @@ public class BettingService {
         this.raceProcessor = raceProcessor;
     }
 
-    public void playOneRound(Vehicles vehicles) {
+    public void playOneRound(Vehicles vehicles, PredictedWinner predictedWinner) {
         List<RaceResultDto> finishedRace = raceProcessor.runRace(vehicles);
         List<RankResultDto> ranksOfRace = raceProcessor.statisticsOf(vehicles);
         List<String> winners = raceProcessor.findWinners(ranksOfRace);
+        boolean isSuccess = winners.contains(predictedWinner.name());
     }
 }
