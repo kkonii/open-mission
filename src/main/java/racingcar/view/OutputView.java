6eb67624b7574daaf1d01133c824cc0d0afe70ad
kkonii <119216472+kkonii@users.noreply.github.com>
2025-11-24 18:27:26 +0900

    refactor: 배팅 성공시 출력되는 문구 내용 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -56,14 +56,13 @@ public class OutputView {
         System.out.println(formatOf(MessageKey.WINNER_IS, String.join(JOINING_DELIMITER, bettingRound.winnerNames())));
         if (bettingRound.isSuccess()) {
             System.out.println(provider.messageOf(MessageKey.BETTING_SUCCESS));
-            System.out.println();
             return;
         }
         System.out.println(provider.messageOf(MessageKey.BETTING_FAIL));
-        System.out.println();
     }
 
     public void printWinRate(WinRateDto winRate) {
+        System.out.println();
         printWithBanner(provider.messageOf(MessageKey.WIN_RATE_HEADER));
         System.out.println(formatOf(MessageKey.WIN_RATE, winRate.value()));
     }
